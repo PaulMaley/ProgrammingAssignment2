8e1cf53ef7d8d195c2ecc92840a05b387e92cf40
Paul Maley <maleyenator@gmail.com>
2014-04-20 16:55:58 +0200

    My implementation

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,51 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Functions for creating a cached matrix and 
+## its inverse - based on pattern provided. 
 
-## Write a short comment describing this function
+## Constructor function
+## x: A matrix
+## function returns an object (a list) containing
+## a set of functions .. 
 
 makeCacheMatrix <- function(x = matrix()) {
+  ## We assume that the matrix is invertible !
 
+  ## xi: local variable that will form part of the environment 
+  ## of the environment for set(), get(), etc.. 
+  ## x is an argument so is already defined in this environment
+  xi <- NULL     ## Inverse matrix
+
+  set <- function(y) {
+    x <<- y      ## Contents of cached matrix may be changed after construction
+    xi <<- NULL  ## if matrix is reset then cached inverse is no longer valid
+  }
+  get <- function() x
+  setInverse <- function(inverse) xi <<- inverse
+  getInverse <- function() xi
+  
+  ## return an object (list)
+  list(set=set, get=get, setInverse=setInverse, getInverse=getInverse)
 }
 
 
-## Write a short comment describing this function
+## Function which takes a CacheMatrix object as 
+## created by the above function.
+## It returns the inverse of the matrix either
+## by solving the matrix or by returning a 
+## previously calculated and cached version
+## (Note that it returns a matrix not a CacheMatrix)
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+  ## Return a matrix that is the inverse of 'x'
+  xi <- x$getInverse()
+  
+  if ( !is.null(xi) ) {
+    message("Using cached version")
+    return(xi) 
+  }
+  
+  xi <- solve(x$get())
+  x$setInverse(xi)
+  xi
 }
+
+
